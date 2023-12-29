874b59eb154739005f46eca17d09e764b81b6947
loginn <loginn>
2023-12-29 20:06:56 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { Field, Formik, Form } from "formik";
+const init = {
+  username: "Dana",
+  password: "123",
+};
 function App() {
+  function x(values) {
+    console.log(values);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Formik initialValues={init} onSubmit={x}>
+        <Form>
+          <div>
+            <label>Name</label>
+            <Field type="email" name="username" />
+          </div>
+          <div>
+            <label>Password</label>
+            <Field type="password" name="password" />
+          </div>
+          <button>Login</button>
+        </Form>
+      </Formik>
     </div>
   );
 }
